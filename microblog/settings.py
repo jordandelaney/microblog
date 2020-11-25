7ea949705f48e2422e542c83cbaaf57dfed5fd89
Jordan Delaney <jordan.delaney@me.com>
2020-11-25 14:00:13 -0500

    Heroku fix

--- microblog/settings.py
+++ microblog/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
     'localhost',
-    'jd-miniblog.herokuapp.com', 
+    'jd-microblog.herokuapp.com', 
     '127.0.0.1',
 ]
 
